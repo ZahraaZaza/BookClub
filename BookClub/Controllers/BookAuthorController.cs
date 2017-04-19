72718afcf6a88efe05a1b60fbbca28e812678f96
Zahraa <1537595@2F18-11>
2017-04-19 18:12:03 -0400

    added the book club mvc

---
+++ BookClub/Controllers/BookAuthorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookClub.Controllers
+{
+    public class BookAuthorController : Controller
+    {
+        // GET: BookAuthor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
