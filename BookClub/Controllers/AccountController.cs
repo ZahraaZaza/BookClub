69e35c5fa6d146404fd6211953a82e89b17c7b9c
1010290 <1010290@2F14-25>
2017-05-05 14:29:54 -0400

    Renamed the AccountController Index to Login, the layout index to login, the index name to login, and in the webconfig file

--- BookClub/Controllers/AccountController.cs
+++ BookClub/Controllers/AccountController.cs
@@ -11,7 +11,7 @@ namespace BookClub.Controllers
     public class AccountController : Controller
     {
         // GET: Account
-        public ActionResult Index(string returnUrl)
+        public ActionResult Login(string returnUrl)
         {
             ViewBag.ReturnUrl = returnUrl;
             return View();
@@ -19,7 +19,7 @@ namespace BookClub.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Index([Bind(Include = "UserName, Password")] User userIn, string ReturnUrl)
+        public ActionResult Login([Bind(Include = "UserName, Password")] User userIn, string ReturnUrl)
         {
             if (ModelState.IsValid)
             {
